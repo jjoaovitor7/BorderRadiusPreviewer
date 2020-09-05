e84977eef1c1f8c57051d50bd8a5baf2a69bbcbb
João Vítor <joaovitorsf7@hotmail.com>
2020-09-05 11:20:11 -0300

    parte do código sendo gerado.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const inputBorder   = document.getElementById("vborder");
 const previewBorder = document.querySelector(".preview");
+const code          = document.querySelector(".code");
 
 function keyEnter(e) {
     let key = e.keyCode;
@@ -8,6 +9,8 @@ function keyEnter(e) {
         // console.log(inputBorder.value);
         // console.log(previewBorder)
         previewBorder.style.borderRadius = inputBorder.value + "px";
+
+        code.innerHTML = "border-radius: " + inputBorder.value + "px" + ";";
     }
 }
 

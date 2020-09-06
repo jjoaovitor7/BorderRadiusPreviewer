ec0e4870f38f51c5747d7f97fd52a9c62af0c47d
João Vítor <joaovitorsf7@hotmail.com>
2020-09-06 19:39:05 -0300

    fix.

--- js/reset.js
+++ js/reset.js
@@ -1,5 +1,4 @@
 const buttonReset   = document.getElementById("reset-border");
-const previewBorder = document.querySelector(".preview");
 
 buttonReset.addEventListener("click", function(e){
     previewBorder.style.borderRadius = "0px";

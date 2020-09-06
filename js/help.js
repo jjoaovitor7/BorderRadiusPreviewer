a71777b43582de9a50885167f8f1592436b49551
João Vítor <joaovitorsf7@hotmail.com>
2020-09-06 20:19:54 -0300

    double click para esconder o texto.

--- js/help.js
+++ js/help.js
@@ -2,7 +2,12 @@ const imgHelp  = document.querySelector(".img-help");
 const spanHelp = document.querySelector(".span-help");
 
 function showText(e) {
-    spanHelp.style.display = "block";
+    spanHelp.style.display = "flex";
+}
+
+function hiddenText(e) {
+    spanHelp.style.display = "none";
 }
 
 imgHelp.addEventListener("click", showText);
+imgHelp.addEventListener("dblclick", hiddenText);

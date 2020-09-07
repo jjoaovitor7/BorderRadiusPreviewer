5812d853aac7edb9dbcacaa93cd3a53b0ae5b93f
João Vítor <joaovitorsf7@hotmail.com>
2020-09-07 10:45:54 -0300

    funcionalidade para os 4 cantos e inputs.

--- js/main.js
+++ js/main.js
@@ -1,89 +1,31 @@
-const inputBorder   = document.getElementById("vborder");
+const inputBorder1   = document.getElementById("vTopLeft");
+const inputBorder2   = document.getElementById("vTopRight");
+const inputBorder3   = document.getElementById("vBottomLeft");
+const inputBorder4   = document.getElementById("vBottomRight");
+
 const previewBorder = document.querySelector(".preview");
-const code          = document.querySelector(".code");
-const chkCss3       = document.getElementById("css3");
-const chkMoz        = document.getElementById("moz");
-const chkWebkit     = document.getElementById("webkit");
 
 function keyEnter(e) {
     let key = e.which || e.keyCode;
     let keyEnterPressed = (key == 13);
 
-    let inputBorderValue = inputBorder.value;
-    let veryHighValue = inputBorderValue > 9999;
-    let veryLowValue  = inputBorderValue < -9999;
-
-    let css3Checked   = chkCss3.checked == true;
-    let mozChecked    = chkMoz.checked == true;
-    let webkitChecked = chkWebkit.checked == true;
-
-    let css3NotChecked   = chkCss3.checked == false;
-    let mozNotChecked    = chkMoz.checked == false;
-    let webkitNotChecked = chkWebkit.checked == false;
-
+    let inputBorderValue1 = inputBorder1.value;
+    let inputBorderValue2 = inputBorder2.value;
+    let inputBorderValue3 = inputBorder3.value;
+    let inputBorderValue4 = inputBorder4.value;
 
     if(keyEnterPressed) {
-        // console.log(inputBorder.value);
-        // console.log(previewBorder)
 
-        if (veryHighValue)
-        {
-            previewBorder.style.borderRadius = 9999 + "px";
-            code.innerHTML = "border-radius: " + 9999 + "px;";
-        }
+        previewBorder.style.borderTopLeftRadius = inputBorderValue1  + "px";
+        previewBorder.style.borderTopRightRadius = inputBorderValue2  + "px";
+        previewBorder.style.borderBottomLeftRadius = inputBorderValue3  + "px";
+        previewBorder.style.borderBottomRightRadius = inputBorderValue4  + "px";
 
-        else if (veryLowValue)
-        {
-            previewBorder.style.borderRadius = -9999 + "px";
-            code.innerHTML = "border-radius: " + -9999 + "px;";
-        }
-
-        else {
-
-            previewBorder.style.borderRadius = inputBorderValue + "px";
-
-            if (css3Checked && mozNotChecked && webkitNotChecked) 
-            {
-                code.innerHTML = "border-radius: " + inputBorderValue + "px;";
-            }
-
-            else if (mozChecked && css3NotChecked && webkitNotChecked)
-            {
-                code.innerHTML = "-moz-border-radius: " + inputBorderValue + "px;";
-            }
-
-            else if (webkitChecked && mozNotChecked && css3NotChecked)
-            {
-                code.innerHTML = "-webkit-border-radius: " + inputBorderValue + "px;";            
-            }
-
-            else if (css3Checked && mozChecked && webkitNotChecked)
-            {
-                code.innerHTML = "border-radius: " + inputBorderValue + "px;";
-                code.innerHTML += "<br/>-moz-border-radius: " + inputBorderValue + "px;";
-            }
-            
-            else if (css3Checked && mozNotChecked && webkitChecked)
-            {
-                code.innerHTML = "border-radius: " + inputBorderValue + "px;";
-                code.innerHTML += "<br/>-webkit-border-radius: " + inputBorderValue + "px;";
-            }
-
-            else if (css3NotChecked && mozChecked && webkitChecked)
-            {
-                code.innerHTML = "-moz-border-radius: " + inputBorderValue + "px;";
-                code.innerHTML += "<br/>-webkit-border-radius: " + inputBorderValue + "px;";
-            }
-
-
-            else if (css3Checked && mozChecked && webkitChecked)
-            {
-                code.innerHTML = "border-radius: " + inputBorderValue + "px;";
-                code.innerHTML += "<br/>-moz-border-radius: " + inputBorderValue + "px;";
-                code.innerHTML += "<br/>-webkit-border-radius: " + inputBorderValue + "px;";
-            }
-        }
     }
+
 }
 
-inputBorder.addEventListener("keydown", keyEnter);
+inputBorder1.addEventListener("keydown", keyEnter);
+inputBorder2.addEventListener("keydown", keyEnter);
+inputBorder3.addEventListener("keydown", keyEnter);
+inputBorder4.addEventListener("keydown", keyEnter);

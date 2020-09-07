9121d924950c7e456fc4a56cda6366a5361beba4
João Vítor <joaovitorsf7@hotmail.com>
2020-09-07 18:37:33 -0300

    inputs resetados ao clicar no botão de resetar.

--- js/reset.js
+++ js/reset.js
@@ -1,5 +1,9 @@
 const buttonReset = document.getElementById("btn-reset");
 
 buttonReset.addEventListener("click", function(e){
+    inputBorder1.value = 0;
+    inputBorder2.value = 0;
+    inputBorder3.value = 0;
+    inputBorder4.value = 0;
     previewBorder.style.borderRadius = "0px";
 });

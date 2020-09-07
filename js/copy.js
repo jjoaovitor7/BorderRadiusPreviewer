7acc244be440dcee71b9fdba0ec3ec759be7b65b
João Vítor <joaovitorsf7@hotmail.com>
2020-09-07 19:07:13 -0300

    botão de copiar e sua funcionalidade.

---
+++ js/copy.js
@@ -0,0 +1,12 @@
+function copyCode(){
+    let text = code.innerText;
+    let elem = document.createElement("textarea"); // elem -> element
+
+    document.body.appendChild(elem);
+    elem.value = text;
+
+    elem.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(elem);
+}

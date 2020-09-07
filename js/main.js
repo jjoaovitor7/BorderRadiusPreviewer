ba658ac85532af64316a0dcd8fab5ddc898025d3
João Vítor <joaovitorsf7@hotmail.com>
2020-09-07 12:20:06 -0300

    funcionalidade do code e valor padrão nos inputs.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ const inputBorder4   = document.getElementById("vBottomRight");
 
 const previewBorder = document.querySelector(".preview");
 
+const code          = document.querySelector(".code");
+const chkCss3       = document.getElementById("css3");
+const chkMoz        = document.getElementById("moz");
+const chkWebkit     = document.getElementById("webkit");
+
 function keyEnter(e) {
     let key = e.which || e.keyCode;
     let keyEnterPressed = (key == 13);
@@ -14,6 +19,10 @@ function keyEnter(e) {
     let inputBorderValue3 = inputBorder3.value;
     let inputBorderValue4 = inputBorder4.value;
 
+    let css3Checked   = chkCss3.checked == true;
+    let mozChecked    = chkMoz.checked == true;
+    let webkitChecked = chkWebkit.checked == true;
+
     if(keyEnterPressed) {
 
         previewBorder.style.borderTopLeftRadius = inputBorderValue1  + "px";
@@ -21,6 +30,93 @@ function keyEnter(e) {
         previewBorder.style.borderBottomLeftRadius = inputBorderValue3  + "px";
         previewBorder.style.borderBottomRightRadius = inputBorderValue4  + "px";
 
+        if (css3Checked && !(mozChecked) && !(webkitChecked)) 
+        {
+            code.innerHTML = "border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+        }
+
+        else if (mozChecked && !(css3Checked) && !(webkitChecked))
+        {
+            code.innerHTML = "-moz-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+        }
+
+        else if (webkitChecked && !(mozChecked) && !(css3Checked))
+        {
+            code.innerHTML = "-webkit-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";           
+        }
+
+        else if (css3Checked && mozChecked && !(webkitChecked))
+        {
+            code.innerHTML = "border-radius: " 
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+            code.innerHTML += "<br/>-moz-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+        }
+        
+        else if (css3Checked && !(mozChecked) && webkitChecked)
+        {
+            code.innerHTML = "border-radius: " 
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+            code.innerHTML += "<br/>-webkit-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+        }
+
+        else if (!(css3Checked) && mozChecked && webkitChecked)
+        {
+            code.innerHTML = "-moz-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+            code.innerHTML += "<br/>-webkit-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+        }
+
+
+        else if (css3Checked && mozChecked && webkitChecked)
+        {
+            code.innerHTML = "border-radius: " 
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+            code.innerHTML += "<br/>-moz-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";
+            code.innerHTML += "<br/>-webkit-border-radius: "
+                                    + inputBorderValue1 + "px "
+                                    + inputBorderValue2 + "px "
+                                    + inputBorderValue3 + "px "
+                                    + inputBorderValue4 + "px;";        }
     }
 
 }

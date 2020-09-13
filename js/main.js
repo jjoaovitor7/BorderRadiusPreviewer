78b03c56c2fe1384021289a71f4542860815cd7f
João Vítor <joaovitorsf7@hotmail.com>
2020-09-13 12:25:26 -0300

    removido change_border.js e documentação.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@
  * a declaração dos inputs e da função process, (com exceç-
  * ão do reset, que o arquivo responsável é o reset.js),
  * verificando se a tecla pressionado foi o Enter/Return e
- * acionando as funções changeBorder(params) do arquivo
- * change_border.js e codeBox(params) do arquivo code.js e
- * também adicionando os eventos de clique da tecla nos in-
- * puts.
+ * acionando as funções changeBorder(params) do mesmo arqu-
+ * ivo e codeBox(params) do arquivo code.js e também adici-
+ * onando os eventos de clique da tecla nos inputs.
  */
 
 const inputBorder1 = document.getElementById("vTopLeft");
@@ -14,7 +13,23 @@ const inputBorder2 = document.getElementById("vTopRight");
 const inputBorder3 = document.getElementById("vBottomLeft");
 const inputBorder4 = document.getElementById("vBottomRight");
 
+const preview = document.querySelector(".preview");
+function changeBorder(input1, input2, input3, input4) {
+  /**
+   * Função responsável pela alteração do border-radius do preview.
+   */
+  preview.style.borderTopLeftRadius = input1 + "px";
+  preview.style.borderBottomLeftRadius = input3 + "px";
+  preview.style.borderTopRightRadius = input2 + "px";
+  preview.style.borderBottomRightRadius = input4 + "px";
+}
+
 function process(e) {
+  /**
+   * Função para verificação da tecla pressionada e
+   * da execução das funções auxiliares:
+   * changeBorder(params) e codeBox(params).
+   */
   let key = e.which || e.keyCode;
   let keyEnterPressed = key == 13;
 

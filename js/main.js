c953856d3b9b3e2b7665969e4fc4294eb229c1a9
João Vítor <joaovitorsf7@hotmail.com>
2020-09-25 17:17:15 -0300

    remoção do btn enviar e alteração em "tempo real".

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 /** pt_BR
  * Este é o arquivo principal da aplicação, é nele que está
  * a declaração dos inputs e da função process, (com exceç-
- * ão do reset, que o arquivo responsável é o reset.js),
- * verificando se a tecla pressionado foi o Enter/Return e
- * acionando as funções changeBorder(params) do mesmo arqu-
+ * ão do reset, que o arquivo responsável é o reset.js), o
+ * acionamento das funções changeBorder(params) do mesmo arqu-
  * ivo e codeBox(params) do arquivo code.js e também adici-
- * onando os eventos de clique da tecla nos inputs.
+ * onando os eventos nos inputs.
  */
 
 const inputBorder1 = document.getElementById("vTopLeft");
@@ -25,37 +24,28 @@ function changeBorder(input1, input2, input3, input4) {
 }
 
 function process(e) {
-  /**
-   * Função para verificação da tecla pressionada e
-   * da execução das funções auxiliares:
-   * changeBorder(params) e codeBox(params).
-   */
-  let key = e.which || e.keyCode;
-  let keyEnterPressed = key == 13;
 
   let inputBorderValue1 = inputBorder1.value;
   let inputBorderValue2 = inputBorder2.value;
   let inputBorderValue3 = inputBorder3.value;
   let inputBorderValue4 = inputBorder4.value;
 
-  if (keyEnterPressed || e.buttons == 0) {
-    changeBorder(
-      inputBorderValue1,
-      inputBorderValue2,
-      inputBorderValue3,
-      inputBorderValue4
-    );
+  changeBorder(
+    inputBorderValue1,
+    inputBorderValue2,
+    inputBorderValue3,
+    inputBorderValue4
+  );
 
-    codeBox(
-      inputBorderValue1,
-      inputBorderValue2,
-      inputBorderValue3,
-      inputBorderValue4
-    );
-  }
+  codeBox(
+    inputBorderValue1,
+    inputBorderValue2,
+    inputBorderValue3,
+    inputBorderValue4
+  );
 }
 
-inputBorder1.addEventListener("keydown", process);
-inputBorder2.addEventListener("keydown", process);
-inputBorder3.addEventListener("keydown", process);
-inputBorder4.addEventListener("keydown", process);
+inputBorder1.addEventListener("change", process);
+inputBorder2.addEventListener("change", process);
+inputBorder3.addEventListener("change", process);
+inputBorder4.addEventListener("change", process);

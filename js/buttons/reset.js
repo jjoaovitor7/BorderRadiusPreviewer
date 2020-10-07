269087b5aca33d449773ad9abd29552c3d300752
João Vítor <joaovitorsf7@hotmail.com>
2020-10-07 14:11:49 -0300

    refatoração.

--- js/buttons/reset.js
+++ js/buttons/reset.js
@@ -2,18 +2,23 @@
 import codeBox from "../codeBox.js";
 ///////////////////
 
-
 function resetAll(e) {
   /**
    * Função responsável por resetar todos os valores, em outras palavras,
    * setar os inputs para o valor 0, setar o border-radius do preview pa-
    * ra 0 e setar um código de implementação com os valores setados em 0.
    */
+  ///// inputs /////
+  const inputBorder1 = document.getElementById("border-top-left");
+  const inputBorder2 = document.getElementById("border-bottom-left");
+  const inputBorder3 = document.getElementById("border-top-right");
+  const inputBorder4 = document.getElementById("border-bottom-right");
+  //////////////////
 
-  document.getElementById("border-top-left").value = 0;
-  document.getElementById("border-bottom-left").value = 0;
-  document.getElementById("border-top-right").value = 0;
-  document.getElementById("border-bottom-right").value = 0;
+  inputBorder1.value = 0;
+  inputBorder2.value = 0;
+  inputBorder3.value = 0;
+  inputBorder4.value = 0;
 
   document.querySelector(".preview").style.borderRadius = "0px";
 

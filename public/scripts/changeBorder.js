b7c9b14dac88de9abfb10e2bbd200ed24b355a16
João Vítor <joaovitorsf7@hotmail.com>
2020-11-03 17:26:05 -0300

    fix e modificando  p/ template string.

--- public/scripts/changeBorder.js
+++ public/scripts/changeBorder.js
@@ -3,11 +3,11 @@ function changeBorder(input1, input2, input3, input4) {
    * Função responsável pela alteração do border-radius do preview.
    */
 
-  const preview = document.querySelector(".preview");
-  preview.style.borderTopLeftRadius = input1 + "px";
-  preview.style.borderBottomLeftRadius = input3 + "px";
-  preview.style.borderTopRightRadius = input2 + "px";
-  preview.style.borderBottomRightRadius = input4 + "px";
+  const previewStyle = document.querySelector(".preview").style;
+  previewStyle.borderTopLeftRadius = `${input1}px`;
+  previewStyle.borderBottomLeftRadius = `${input2}px`;
+  previewStyle.borderTopRightRadius = `${input3}px`;
+  previewStyle.borderBottomRightRadius = `${input4}px`;
 }
 
 export default changeBorder;

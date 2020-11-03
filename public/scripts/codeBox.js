6b255f221164003c11633be97f083b04f7f12ebe
João Vítor <joaovitorsf7@hotmail.com>
2020-11-03 17:15:53 -0300

    modificando p/ template string.

--- public/scripts/codeBox.js
+++ public/scripts/codeBox.js
@@ -16,171 +16,62 @@ function codeBox(input1, input2, input3, input4) {
 
   const code = document.querySelector(".code");
 
-  if (input1 == ""){
+  if (input1 == "") {
     input1 = 0;
   }
-  
-  if (input2 == ""){
+
+  if (input2 == "") {
     input2 = 0;
   }
 
-  if (input3 == ""){
+  if (input3 == "") {
     input3 = 0;
   }
 
-  if (input4 == ""){
+  if (input4 == "") {
     input4 = 0;
   }
 
   // se o checkbox do css3 estiver checkado
   if (CSS3Checked && !MOZChecked && !WEBKITChecked) {
-    code.innerHTML =
-      "border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
+    code.innerHTML = `border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
   }
 
   // se o checkbox do moz estiver checkado
   else if (MOZChecked && !CSS3Checked && !WEBKITChecked) {
-    code.innerHTML =
-      "-moz-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
+    code.innerHTML = `-moz-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
   }
 
   // se o checkbox do webkit estiver checkado
   else if (WEBKITChecked && !MOZChecked && !CSS3Checked) {
-    code.innerHTML =
-      "-webkit-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
+    code.innerHTML = `-webkit-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
   }
 
   // se o checkbox do css3 e do moz estiverem checkados
   else if (CSS3Checked && MOZChecked && !WEBKITChecked) {
-    code.innerHTML =
-      "border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
-    code.innerHTML +=
-      "<br/>-moz-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
+    code.innerHTML = `border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
+    code.innerHTML += `<br/>-moz-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
   }
 
   // se o checkbox do css3 e do webkit estiverem checkados
   else if (CSS3Checked && !MOZChecked && WEBKITChecked) {
-    code.innerHTML =
-      "border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
-    code.innerHTML +=
-      "<br/>-webkit-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
+    code.innerHTML = `border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
+    code.innerHTML += `<br/>-webkit-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
   }
 
   // se o checkbox do moz e do webkit estiverem checkados
   else if (!CSS3Checked && MOZChecked && WEBKITChecked) {
-    code.innerHTML =
-      "-moz-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
-    code.innerHTML +=
-      "<br/>-webkit-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
+    code.innerHTML = `-moz-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
+    code.innerHTML += `<br/>-webkit-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
   }
 
   // se todos os checkboxes estiverem checkados
   else if (allCheckboxesChecked) {
-    code.innerHTML =
-      "border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
-    code.innerHTML +=
-      "<br/>-moz-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
-    code.innerHTML +=
-      "<br/>-webkit-border-radius: " +
-      input1 +
-      "px " +
-      input2 +
-      "px " +
-      input3 +
-      "px " +
-      input4 +
-      "px;";
-  }
-  else {
-    code.innerHTML = '&nbsp;';
+    code.innerHTML = `border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
+    code.innerHTML += `<br/>-moz-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
+    code.innerHTML += `<br/>-webkit-border-radius: ${input1}px ${input2}px ${input3}px ${input4}px;`;
+  } else {
+    code.innerHTML = "&nbsp;";
   }
 }
 
